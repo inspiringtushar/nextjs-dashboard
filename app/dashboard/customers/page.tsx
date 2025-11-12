cd157bae532534e4f5c0db58efcc33250b540ccc
Tushar Mangla <tusharmangla1997@gmail.com>
2025-11-12 13:08:23 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+function CustomerPage() {
+    return <h1>Customer Page</h1>;
+}
+
+export default CustomerPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+function InvoicePage() {
+    return <h1>Invoice Page</h1>;
+}
+
+export default InvoicePage;
